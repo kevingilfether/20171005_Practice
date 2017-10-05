f06c29f48451a2afd878de483ef51a02d8f978f4
kevingilfether <kevingilfether@gmail.com>
2017-10-05 16:20:27 -0400

    loops to make multiplication

--- 20171005 Practice/Program.cs
+++ 20171005 Practice/Program.cs
@@ -10,6 +10,20 @@ namespace _20171005_Practice
     {
         static void Main(string[] args)
         {
+            //using debug to check nested loops
+
+            // Make a multiplication table
+
+            for (int i = 1; i<=5; i++)
+            {
+                Console.Write(i + "\t");                
+                for (int j = 1; j <=5; j++)
+                {
+                    Console.Write("\t" + j * i);
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
